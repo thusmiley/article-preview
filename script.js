372908e100cc7e56384ddff25158a4ff073ab543
Thu Dang <thudang.dtt@gmail.com>
2021-08-12 10:58:08 -0500

    final

---
+++ script.js
@@ -0,0 +1,8 @@
+function showPopup() {
+  let popup = document.getElementById("popup");
+  if (popup.style.display === "flex") {
+    popup.style.display = "none";
+  } else {
+    popup.style.display = "flex";
+  }
+}
